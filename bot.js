6ddb316bd069bf186b26cc3549386b1a65362fdf
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-21 21:26:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ bot.on('ready', function (evt) {
   bot.on('message', function (user, userID, channelID, message, evt) {
     //Our bot needs to know if it will execute a command
     //It will listen for messages that  will start with '|'
-    if (message.substring(0,1) == '|') {
+    if (message.substring(0,1) == '|' && !message.author.bot) {
       var args = message.substring(1).split(' ');
       var cmd = args[0];
       
