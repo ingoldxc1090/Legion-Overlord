d7d502b1922fd929cd64dcb2e8feab9ba49c0b9d
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-21 22:21:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
+var auth = require('./config.json');
+const fs = require("fs");
 //Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -20,7 +21,7 @@ bot.on('ready', function (evt) {
   bot.on('message', function (user, userID, channelID, message, evt) {
     //Our bot needs to know if it will execute a command
     //It will listen for messages that  will start with '|'
-    if (message.substring(0,1) == '|' && !message.author.bot) {
+    if (message.substring(0,1) == config.prefix && !message.author.bot) {
       var args = message.substring(1).split(' ');
       var cmd = args[0];
       
@@ -31,6 +32,14 @@ bot.on('ready', function (evt) {
           bot.sendMessage({
             to: channelID,
             message: 'Pong!'
+          });
+        case 'prefix':
+          let newPrefix = args.splice(2);
+          config.prefix = newPrefix;
+          fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error)
+          bot.sendMessage({
+            to: channelID,
+            message: 'Changed prefix to ' + newPrefix
           });
         break;
       }
