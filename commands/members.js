cc04b1e37e0300c941237a1c1e30d8824097ac75
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-22 21:15:08 -0400

    Created members command

---
+++ commands/members.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+exports.run = (client, message) => {
+	var arrayMembers = message.guild.members.array();
+	var botCount = 0;
+	var humanCount = 0;
+	for (i = 0; i < arrayMembers.length; i++) {
+		if (arrayMembers[i].user.bot) {
+			botCount++;
+		} else {
+			humanCount++;
+		}
+	}
+	const embed = new Discord.RichEmbed()
+		.addField("Member Count", message.guild.memberCount)
+		.addField("Humans", humanCount)
+		.addField("Bots", botCount);
+	message.channel.send({embed});
+}
