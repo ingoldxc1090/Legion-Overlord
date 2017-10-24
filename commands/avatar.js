d48aa8b9dc7fb80874a675103c4ef5bef72cf310
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-24 01:40:59 -0400

    improved avatar and bomb commands

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,8 +1,13 @@
-exports.run = (client, message, args) => {
+exports.run = (client, message, args) =>
+{
     if (args.length < 1) {
         message.channel.send(message.author.avatarURL);
     } else {
-        let member = message.mentions.members.first().user;
-        message.channel.send(member.avatarURL);
+        let member = message.mentions.members.first();
+        if (member == null) {
+            message.channel.send("You must mention a user.")
+        } else {
+            message.channel.send(member.user.avatarURL);
+        }
     }
 }
--- commands/bomb.js
+++ commands/bomb.js
@@ -1,16 +1,16 @@
 exports.run = (client, message, args) => {
     var connectedChannel;
     if (args.length < 1) {
-        connectedChannel = message.member.voiceChannelID;
+        connectedChannel = message.member.voiceChannel;
     } else {
-        connectedChannel = message.mentions.members.first().voiceChannelID;
+        connectedChannel = message.mentions.members.first().voiceChannel;
     }
     var connectedUsers = [];
     var voiceChannels = [];
     var arrayChannels = message.guild.channels.array();
     var arrayMembers = message.guild.members.array();
     for (i = 0; i < arrayMembers.length; i++) {
-        if (arrayMembers[i].voiceChannelID == connectedChannel) {
+        if (arrayMembers[i].voiceChannel === connectedChannel) {
             connectedUsers[connectedUsers.length] = arrayMembers[i];
         }
     }
@@ -21,7 +21,7 @@ exports.run = (client, message, args) => {
     }
     for (i = 0; i< connectedUsers.length; i++) {
         var rand = Math.floor(Math.random()*voiceChannels.length);
-        if (voiceChannels[rand].id != connectedChannel) {
+        if (voiceChannels[rand].id != connectedChannel && voiceChannels[rand].permissionsFor(connectedUsers[i]).has(0x00100000)) {
             connectedUsers[i].setVoiceChannel(voiceChannels[rand]);
             message.channel.send(`${connectedUsers[i]} was thrown to ${voiceChannels[rand]}`);
         } else {
