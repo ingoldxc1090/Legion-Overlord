9131abc6824bf622fbb5c396249a5fac2ff20f3d
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-26 21:32:33 -0400

    Completed warn command

--- commands/warn.js
+++ commands/warn.js
@@ -1,18 +1,76 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 const punishment = require('./commandData/punishment.json');
+const permissionLevel = require('../subfunctions/permissionLevel');
 exports.run = (client, message, args, config) => {
-    if(permissionLevel(client, message, config, 2)) {
-        for(i = 0; i < punishment.offenders.length; i++) {
-            if(args.mentions.users.first().id == punishment.offenders[i]) {
-                punishment.warnCount[i]++;
-                if(punishment.warnCount > 3) {
-                    let mute = require('./mute.js');
-                    mute.run(client, message, args, config);
-                    return;
-                }
-                
+    if(permissionLevel.run(client, message, config, 2)) {
+        var serverMembers = message.guild.members.array();
+        var user;
+        for(i = 0; i < serverMembers.length; i++) {
+            if(serverMembers[i].user.id === args[0].substr(2).slice(0,-1)) {
+                user = serverMembers[i].user;
             }
         }
+        var reason = args[1];
+        delete args[0];
+        delete args[1];
+        var evidence = args.join(' ');
+        const moderationLog = message.guild.channels.find('name', 'moderation_log');
+        for(i = 0; i < punishment.offenders.length; i++) {
+            if(user.id == punishment.offenders[i]) {
+                punishment.warnCount[i]++;
+                if(punishment.warnCount[i] > 3) {
+                    let mute = require('./mute.js');
+                    //mute.run(client, message, args, config);
+                    punishment.warnCount[i] = 0;
+                    fs.writeFile("./commandData/bomb.json", JSON.stringify(punishment), (err) => console.error);
+                    return;
+                } else {
+                    fs.writeFile("./commandData/bomb.json", JSON.stringify(punishment), (err) => console.error);
+                    const DMembed = new Discord.RichEmbed()
+                        .setTitle("Warning Notification")
+                        .setColor(0xff7f00)
+                        .setDescription(`You have received **${punishment.warnCount[i]}/3** infractions.\n3 will result in a temporary mute and a step towards the next punishment.`)
+                        .addField("Reason", reason)
+                        .addField("Evidence", evidence)
+                        .setTimestamp()
+                        .setFooter("Contact an admin if you feel you have been wrongfully punished");
+                    user.send(DMembed);
+                    const logEmbed = new Discord.RichEmbed()
+                        .setTitle("User Warned")
+                        .setDescription(user)
+                        .setColor(0xff7f00)
+                        .addField("Reason", reason)
+                        .addField("Evidence", evidence)
+                        .setTimestamp();
+                    moderationLog.send(logEmbed);
+                    return;
+                }
+            }
+        }
+        punishment.offenders[punishment.offenders.length] = user.id;
+        punishment.warnCount[punishment.warnCount.length] = 1;
+        punishment.muteCount[punishment.muteCount.length] = 0;
+        punishment.kickCount[punishment.kickCount.length] = 0;
+        fs.writeFile("./commandData/punishment.json", JSON.stringify(punishment), (err) => console.error);
+        const DMembed = new Discord.RichEmbed()
+            .setTitle("Warning Notification")
+            .setColor(0xff7f00)
+            .setDescription(`You have received **1/3** infractions.\n3 will result in a temporary mute and a step towards the next punishment.`)
+            .addField("Reason", reason)
+            .addField("Evidence", evidence)
+            .setTimestamp()
+            .setFooter("Contact an admin if you feel you have been wrongfully punished");
+        console.log(user);
+        user.send(DMembed);
+        const logEmbed = new Discord.RichEmbed()
+            .setTitle("User Warned")
+            .setDescription(user)
+            .setColor(0xff7f00)
+            .addField("Reason", reason)
+            .addField("Evidence", evidence)
+            .setTimestamp();
+        moderationLog.send(logEmbed);
+        return;
     }
 }
--- filter/chatFilter.js
+++ filter/chatFilter.js
@@ -1,4 +1,5 @@
-exports.run = (client, message, punishmentCounter) => {
+exports.run = (client, message, punishmentCounter) =>
+{
     //Word Filter
     //Permutation list
     const arrayA = ["a", "@", "e"];
@@ -11,7 +12,7 @@ exports.run = (client, message, punishmentCounter) => {
     const arrayQ = ["q", "p", "g,", "c"];
     const arrayR = ["r", "®"];
     const arrayS = ["s", "$"];
-    const arrayT = ["t","+"];
+    const arrayT = ["t", "+"];
     const arrayU = ["u", "w", "μ", "v"];
     const evidence = message.content;
 
@@ -73,7 +74,7 @@ exports.run = (client, message, punishmentCounter) => {
         for (j = 0; j < arrayA.length; j++) {
             for (k = 0; k < arrayG.length; k++) {
                 var wordyDurd = arrayF[i] + arrayA[j] + arrayG[k];
-                if (message.content.toLowerCase().includes(wordyDurd) && message.content.charAt(message.content.search(wordyDurd)-1) !== ' ') {
+                if (message.content.toLowerCase().includes(wordyDurd) && message.content.charAt(message.content.search(wordyDurd) - 1) !== ' ') {
                     message.delete();
                     console.log("Deleted\n\tOffense Type: f****t\n\tMessage: " + message.content + "\n\tFrom: " + message.author);
                     message.channel.send(message.author + " thou shalt not use such vile language!!!");
@@ -89,14 +90,14 @@ exports.run = (client, message, punishmentCounter) => {
             }
         }
     }
-     
+
     //p*ssy
     for (i = 0; i < arrayP.length; i++) {
         for (j = 0; j < arrayS.length; j++) {
             for (k = 0; k < arrayS.length; k++) {
                 var wordyDurd = arrayP[i] + "u" + arrayS[j] + arrayS[k] + "y";
                 var wordyDurdShort = arrayP[i] + "u" + arrayS[j] + arrayS[k];
-                if (message.content.toLowerCase().includes(wordyDurd) || (message.content.toLowerCase().includes(wordyDurdShort) && message.content.charAt(message.content.search(wordyDurdShort)-1) !== ' ')) {
+                if (message.content.toLowerCase().includes(wordyDurd) || (message.content.toLowerCase().includes(wordyDurdShort) && message.content.charAt(message.content.search(wordyDurdShort) - 1) !== ' ')) {
                     message.delete();
                     console.log("Deleted\n\tOffense Type: p***y\n\tMessage: " + message.content + "\n\tFrom: " + message.author);
                     message.channel.send(message.author + " thou shalt not use such vile language!!!");
@@ -114,13 +115,13 @@ exports.run = (client, message, punishmentCounter) => {
     }
 
     //q*eer
-    for (i = 0; i < arrayQ.length; i++){
-        for (j = 0; j < arrayU.length; j++){
-            for (k = 0; k < arrayE.length; k++){
-                for (l = 0; l < arrayE.length; l++){
-                    for (m = 0; m < arrayR.length; m++){
+/*    for (i = 0; i < arrayQ.length; i++) {
+        for (j = 0; j < arrayU.length; j++) {
+            for (k = 0; k < arrayE.length; k++) {
+                for (l = 0; l < arrayE.length; l++) {
+                    for (m = 0; m < arrayR.length; m++) {
                         var wordyDurd = arrayQ[i] + arrayU[j] + arrayE[k] + arrayE[l] + arrayR[m];
-                        if (message.content.toLowerCase().includes(wordyDurd) || (message.content.toLowerCase().includes(wordyDurdShort) && message.content.charAt(message.content.search(wordyDurdShort)-1) !== ' ')) {
+                        if (message.content.toLowerCase().includes(wordyDurd) || (message.content.toLowerCase().includes(wordyDurdShort) && message.content.charAt(message.content.search(wordyDurdShort) - 1) !== ' ')) {
                             message.delete();
                             console.log("Deleted\n\tOffense Type: p***y\n\tMessage: " + message.content + "\n\tFrom: " + message.author);
                             message.channel.send(message.author + " thou shalt not use such vile language!!!");
@@ -132,9 +133,11 @@ exports.run = (client, message, punishmentCounter) => {
                                 console.error(err);
                             }
                             return;
+                        }
                     }
                 }
             }
         }
     }
+    */
 }
