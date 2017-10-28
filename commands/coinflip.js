72411744ff6e710f50c6c48af3e03afc10dbabfd
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-28 10:04:27 -0400

    more mistakes corrected

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,5 +1,5 @@
 exports.run = (client, message) => {
-    var rand = Math.floor(Math.rand() * 2);
+    var rand = Math.floor(Math.random() * 2);
     if(rand == 0) message.channel.send("It lands on heads.");
     else if (rand == 1) message.channel.send("It lands on tails.");
     else console.error(err);
