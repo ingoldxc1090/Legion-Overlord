c2202a0845df99c6e278713f1d7bcdf0ee260e6e
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-10-28 10:44:45 -0400

    oops missed a couple

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,13 @@
 const config = require("../config.json");
 const Discord = require("discord.js");
 exports.run = (client, message) => {
-    const genCommands = `${config.prefix}ping\n${config.prefix}coinflip\n${config.prefix}diceroll\n${config.prefix}randomnumber\n${config.prefix}randomcolor,\n${config.prefix}avatar\n${config.prefix}members\n${config.prefix}roast\n${config.prefix}bomb\ndefuse`;
+    const genCommands = `${config.prefix}ping\n${config.prefix}coinflip\n${config.prefix}diceroll\n${config.prefix}randomnumber\n${config.prefix}randomcolor\n${config.prefix}avatar\n${config.prefix}members\n${config.prefix}rps\n${config.prefix}roast\n${config.prefix}bomb\n${config.prefix}defuse`;
     const modCommands = `${config.prefix}warn\n${config.prefix}mute\n${config.prefix}kick\n${config.prefix}ban`;
+    const utilCommands = `${config.prefix}prefix`
     const embed = new Discord.RichEmbed()
         .setTitle("Legion Overlord Commands")
         .addField("General", genCommands)
-        .addField("Moderation", modCommands);
+        .addField("Moderation", modCommands)
+        .addField("Utilities", utilCommands);
     message.author.send(embed);
 }
