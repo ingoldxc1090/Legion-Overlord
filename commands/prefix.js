7d5bec03a8bd35ff350ea268052ebb36ff5c97b1
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 10:16:47 -0400

    Added comments

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 const Discord = require("discord.js");
 const permissionLevel = require('../subfunctions/permissionLevel.js');
 exports.run = (client, message, args) => {
-    if(args[0] === "help") {
+    if(args[0] === "help") { //Sends command info for help argument
         const help = new Discord.RichEmbed()
             .setTitle("Prefix")
             .setColor(0xffdf00)
@@ -12,14 +12,13 @@ exports.run = (client, message, args) => {
         message.channel.send(help);
         return;
     }
-	if (!permissionLevel.run(client, message, 3)) return;
-	if (message.content.split(" ").slice(1,2)[0] !== undefined){
-		let newPrefix = message.content.split(" ").slice(1,2)[0];
-		config.prefix = newPrefix;
-		fs.writeFile("../config.json", JSON.stringify(config), (err) => console.error);
-		message.channel.send("Prefix changed to " + newPrefix);
-		console.log("Prefix changed to " + newPrefix);
+	if (!permissionLevel.run(client, message, 3)) return; //Ensures the user has the correct permision level
+	if (message.content.split(" ").slice(1,2)[0] !== undefined){ //Ensures the entered prefix is not blank
+		config.prefix = message.content.split(" ").slice(1,2)[0];
+		fs.writeFile("../config.json", JSON.stringify(config), (err) => console.error); //Writes the new prefix to file
+		message.channel.send("Prefix changed to " + newPrefix); //Sends message announcing change
+		console.log("Prefix changed to " + newPrefix); //Logs change to console
 	} else {
-		message.channel.send("No prefix defined");
+		message.channel.send("No prefix defined"); //Error for no arguments
 	}
 }
