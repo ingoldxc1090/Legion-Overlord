f131b515f20fd09c8f96b039b8055034f38ea991
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 09:52:16 -0400

    Added Comments

--- commands/diceroll.js
+++ commands/diceroll.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    if(args[0] === "help") {
+    if(args[0] === "help") { //Sends command info for help argument
         const help = new Discord.RichEmbed()
             .setTitle("Dice Roll")
             .setColor(0xffdf00)
@@ -10,6 +10,6 @@ exports.run = (client, message, args) => {
         message.channel.send(help);
         return;
     }
-    var roll = Math.floor((Math.random()*6)+1);
-    message.channel.send(`You rolled a ${roll}.`)
+    var roll = Math.floor((Math.random()*6)+1); //Generates a random number between 1 and 6
+    message.channel.send(`You rolled a ${roll}.`); //Sends the generated number
 }
