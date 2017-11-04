b09a040de64984dc322ceb1581bbb90441d5693b
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 09:41:03 -0400

    Added Comments

--- commands/defuse.js
+++ commands/defuse.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const data = require('./commandData/bomb.json');
 const config = require('../config.json');
 const Discord = require("discord.js");
-exports.run = (client, message, args) => {
+exports.run = (client, message, args) => { //Sends command info for help argument
     if(args[0] === "help") {
         const help = new Discord.RichEmbed()
             .setTitle("Defuse")
@@ -12,6 +12,7 @@ exports.run = (client, message, args) => {
         message.channel.send(help);
         return;
     }
+    //Writes to bomb data file to be read before bomb detonates
     data.defused = true;
     fs.writeFile('./commandData/bomb.json', JSON.stringify(data), (err) => console.error);
 }
