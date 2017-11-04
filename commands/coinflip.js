48474e8a365dd213d80f4e6b971ff124bf5ec7a4
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 09:37:19 -0400

    Added Comments

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    if(args[0] === "help") {
+    if(args[0] === "help") { //Sends command info for help argument
             const help = new Discord.RichEmbed()
                 .setTitle("Coin Flip")
                 .setColor(0xffdf00)
@@ -10,8 +10,8 @@ exports.run = (client, message, args) => {
             message.channel.send(help);
             return;
         }
-    var rand = Math.floor(Math.random() * 2);
-    if(rand == 0) message.channel.send("It lands on heads.");
-    else if (rand == 1) message.channel.send("It lands on tails.");
+    var rand = Math.floor(Math.random() * 2); //creates a random number between 0 and 1
+    if(rand == 0) message.channel.send("It lands on heads."); //Message for heads
+    else if (rand == 1) message.channel.send("It lands on tails."); //Meassage for tails
     else console.error(err);
 }
