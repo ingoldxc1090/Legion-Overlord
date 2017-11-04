c234f670a65d1d704fc455df79b99555055f28b0
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 11:07:50 -0400

    Added help argument, comments, and error handling

--- commands/yoda.js
+++ commands/yoda.js
@@ -1,16 +1,27 @@
 const http = require('http');
 exports.run = (client, message, args) => {
-    var text = args.join('%20');
-    http.get(`http://api.funtranslations.com/translate/yoda.json?text=${text}`, (resp) => {
+    if(args[0] === "help") { //Sends command info for help argument
+        const help = new Discord.RichEmbed()
+            .setTitle("Yoda")
+            .setColor(0xffdf00)
+            .setDescription("Translates message into yoda speech.")
+            .addField("Usage", `${config.prefix}yoda {message}`);
+        message.channel.send(help);
+        return;
+    }
+    var text = args.join('%20'); //Merges arguments with space escape character
+    http.get(`http://api.funtranslations.com/translate/yoda.json?text=${text}`, (resp) => { //Makes get request to yoda translator api
         let data = '';
 
-        resp.on('data', (chunk) => {
+        resp.on('data', (chunk) => { //Writes data chunks to variable as they are recieved
             data += chunk;
         });
 
         resp.on('end', () => {
-            message.channel.send(JSON.parse(data).contents.translated);
+            message.channel.send(JSON.parse(data).contents.translated); //Parses response from json and sends it
         });
 
-    })
+    }).on("error", (err) => {
+        console.error(err);
+    });
 }
