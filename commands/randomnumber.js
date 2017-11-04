eaabf4cf86c37bbdeaa423f90ce110937898880b
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 10:38:09 -0400

    Added comments and corrected specified range method

--- commands/randomnumber.js
+++ commands/randomnumber.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    if(args[0] === "help") {
+    if(args[0] === "help") { //Sends command info for help argument
         const help = new Discord.RichEmbed()
             .setTitle("Random Number")
             .setColor(0xffdf00)
@@ -10,12 +10,21 @@ exports.run = (client, message, args) => {
         message.channel.send(help);
         return;
     }
-    if(args.length == 1){
+    if(args[0].isNaN || args[1].isNaN){
+        message.channel.send("Arguments must be numbers.");
+        return;
+    }
+    if(args.length == 1){ //If only one argument argument generates a random number between 1 and the specified value
         var number = Math.floor((Math.random()*args[0])+1);
         message.channel.send(`Your number is ${number}.`);
-    } else if(args.length == 2){
-        var number = Math.floor((Math.random()*args[1])+args[0]);
-        message.channel.send(`Your number is ${number}`)
+    } else if(args.length == 2){ //If two arguments generates a random number between the entered values
+        if(args[1]-args[0] <= 0){
+            message.channel.send("Please enter smaller number first.");
+            return;
+        }
+        var range = args[1]-args[0];
+        var number = Math.floor((Math.random()*range)+args[0]);
+        message.channel.send(`Your number is ${number}`);
     } else {
         message.channel.send("Invalid arguments");
     }
