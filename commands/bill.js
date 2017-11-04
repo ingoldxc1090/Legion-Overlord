836fe9858de497c3ecb2dcd6e3f9063ec6cf57db
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 09:18:30 -0400

    Added comments and help info

--- commands/bill.js
+++ commands/bill.js
@@ -1,6 +1,18 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-    var text = args.join('%20');
+    if(args[0] === "help") { //Sends command info for help argument
+        const help = new Discord.RichEmbed()
+            .setTitle("Bill")
+            .setColor(0xffdf00)
+            .setDescription("Creates a Be Like Bill meme using the entered text.")
+            .addField("Usage", `${config.prefix}bill {memeText}`);
+        message.channel.send(help);
+        return;
+    if(args.length < 1){ //Returns error for no arguments entered
+        message.channel.send("You must enter some text");
+        return;
+    }
+    var text = args.join('%20'); //Merges argments with space escape character for url
     const embed = new Discord.RichEmbed().setImage(`http://belikebill.azurewebsites.net/billgen-API.php?text=This%20is%20Bill%0D%0A%0D%0ABill%20${text}%0D%0A%0D%0ABill%20is%20smart%0D%0A%0D%0ABe%20like%20Bill`)
-    message.channel.send(embed);
+    message.channel.send(embed); //Sends image returned by api call
 }
