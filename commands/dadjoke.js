5ed310387fa77ab32bffb7f33757f082ce6534f6
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 09:39:43 -0400

    Added Comments and help argument

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -1,10 +1,18 @@
 exports.run = (client, message, args) => {
-
+if(args[0] === "help") { //Sends command info for help argument
+        const help = new Discord.RichEmbed()
+            .setTitle("Dad Joke")
+            .setColor(0xffdf00)
+            .setDescription("Returns a random dad joke.")
+            .addField("Usage", `${config.prefix}dadjoke`);
+        message.channel.send(help);
+        return;
+    }
     message.channel.send("This command is currently in development.");
     return;
     const https = require('https');
 
-    const options = {
+    const options = { //Sets request options
         hostname: 'icanhazdadjoke.com',
         path: '/',
         method: 'GET',
@@ -13,14 +21,14 @@ exports.run = (client, message, args) => {
         }
     };
 
-    https.request(options, (resp) => {
+    https.request(options, (resp) => { //Makes request to dad joke api
         let data = '';
 
-    resp.on('data', (chunk) => {
+    resp.on('data', (chunk) => { //Writes data chunks to variable as they are recieved
         data += chunk;
     });
 
-    resp.on('end', () => {
+    resp.on('end', () => { //Sents recieved data
         message.channel.send(data);
     });
 
