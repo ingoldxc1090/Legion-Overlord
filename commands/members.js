a9a99502fae09582d6014d14e204cd8bbeadb537
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 10:08:54 -0400

    Added comments

--- commands/members.js
+++ commands/members.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const config = require('../config.json');
 exports.run = (client, message, args) => {
-    if(args[0] === "help") {
+    if(args[0] === "help") { //Sends command info for help argument
         const help = new Discord.RichEmbed()
             .setTitle("Members")
             .setColor(0xffdf00)
@@ -10,17 +10,17 @@ exports.run = (client, message, args) => {
         message.channel.send(help);
         return;
     }
-	var arrayMembers = message.guild.members.array();
-	var botCount = 0;
+	var arrayMembers = message.guild.members.array(); //Creates an arrary of all memmbers in the guild
+	var botCount = 0; //Initializes variables for scope
 	var humanCount = 0;
-	for (i = 0; i < arrayMembers.length; i++) {
-		if (arrayMembers[i].user.bot) {
+	for (i = 0; i < arrayMembers.length; i++) { //Loops through all members of the guild
+		if (arrayMembers[i].user.bot) { //Indexes botCount if the member is a bot
 			botCount++;
-		} else {
+		} else { //Otherwise indexes humanCount
 			humanCount++;
 		}
 	}
-	const embed = new Discord.RichEmbed()
+	const embed = new Discord.RichEmbed() //Creates embed with returned data
 		.addField("Member Count", message.guild.memberCount)
 		.addField("Humans", humanCount)
 		.addField("Bots", botCount);
