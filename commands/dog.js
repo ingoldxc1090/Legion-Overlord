0c199c3bfba3748486735d75ad0936ad493fb8a9
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-04 09:55:26 -0400

    Added comments and error handling

--- commands/dog.js
+++ commands/dog.js
@@ -1,17 +1,29 @@
 const Discord = require('discord.js');
+const config = require('../config.json');
 const https = require('https');
 exports.run = (client, message, args) => {
-    https.get('https://dog.ceo/api/breeds/image/random', (resp) => {
+    if(args[0] === "help") { //Sends command info for help argument
+        const help = new Discord.RichEmbed()
+            .setTitle("Avatar")
+            .setColor(0xffdf00)
+            .setDescription("Returns the avatar image of the sender or specified user.")
+            .addField("Usage", `${config.prefix}avatar\n${config.prefix}avatar @user`);
+        message.channel.send(help);
+        return;
+    }
+    https.get('https://dog.ceo/api/breeds/image/random', (resp) => { //Makes get request to dog api webpage
         let data = '';
 
-    resp.on('data', (chunk) => {
-        data += chunk;
-});
+        resp.on('data', (chunk) => { //Writes data chunks to variable as they are recieved
+            data += chunk;
+        });
 
-    resp.on('end', () => {
-        const embed = new Discord.RichEmbed().setImage(JSON.parse(data).message);
-        message.channel.send(embed);
-});
+        resp.on('end', () => {
+            const embed = new Discord.RichEmbed().setImage(JSON.parse(data).message); //Parses image url and sets it as the image for a new embed
+            message.channel.send(embed);
+        });
 
-})
+    }).on("error", (err) => {
+        console.error(err);
+    });
 }
