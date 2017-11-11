745fce8670cd16ef5a136cda2166821b60a8ebd7
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-11 11:30:03 -0500

    giverole now gives and removes roles if used again

--- commands/giverole.js
+++ commands/giverole.js
@@ -4,5 +4,9 @@ exports.run = (client, message, args) => {
     var userRole = message.member.highestRole;
     var subjectRole = message.guild.roles.find('name', args.join());
     if(!permissionLevel.run(client, message, 3) || !checkRoles.run(client, userRole, subjectRole)) return;
-    message.member.addRole(message.guild.roles.find('name', args.join()));
+        if(message.member.roles.exists('name', args.join())) {
+            message.member.removeRole(message.guild.roles.find('name', args.join()));
+        } else {
+            message.member.addRole(message.guild.roles.find('name', args.join()));
+        }
 }
