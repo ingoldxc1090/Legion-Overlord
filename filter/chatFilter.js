365d88ed595984474dad4084ecc5e333471788b2
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-11 09:30:43 -0500

    created chatFilter.js as part of filter update

---
+++ filter/chatFilter.js
@@ -0,0 +1,14 @@
+exports.run = (client, message) => {
+    message.delete();
+    console.log("Deleted\n\tOffense Type: Language\n\tMessage: " + text + "\n\tFrom: " + member.user);
+    message.channel.send(member.user + " thou shalt not use such vile language!!!");
+    const user = "**" + member.user.id + "*";
+    const evidence = message.content;
+    var args = [user, 'Language', evidence];
+    try {
+        let warn = require(`../commands/warn.js`);
+        warn.run(client, message, args, true);
+    } catch (err) {
+        console.error(err);
+    }
+}
