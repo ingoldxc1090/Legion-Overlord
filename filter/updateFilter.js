02e5ed361e122f3490efdf3f3ddb6050d8996dfe
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-11 10:35:40 -0500

    created updateFilter.js for new filter system
    reverts user's nickname to previous version and gives them a warning

--- filter/updateFilter.js
+++ filter/updateFilter.js
@@ -1,1 +1,13 @@
-
+exports.run = (client, oldMember, newMember) => {
+    newMember.setNickname(oldMember.nickname);
+    console.log("Reverted Nickname to Previous\n\tOffense Type: Inappropriate Nickname\n\tNickname: " + text + "\n\tFrom: " + member.user);
+    newMember.user.send(newMember.user + " thou shalt not use such vile language!!!");
+    const evidence = newMember.nickname;
+    var args = [newMember, 'Language', evidence];
+    try {
+        let warn = require(`../commands/warn.js`);
+        warn.run(client, null, args, true);
+    } catch (err) {
+        console.error(err);
+    }
+}
