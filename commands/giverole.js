01e1ae69cb035f223511a2e7c4f0cc51480a30a8
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-11 11:42:04 -0500

    @user argument can be specified to give a role to another user

--- commands/giverole.js
+++ commands/giverole.js
@@ -1,12 +1,36 @@
 let permissionLevel = require('../subfunctions/permissionLevel.js');
 let checkRoles = require('../subfunctions/checkRoles.js');
 exports.run = (client, message, args) => {
-    var userRole = message.member.highestRole;
-    var subjectRole = message.guild.roles.find('name', args.join());
-    if(!permissionLevel.run(client, message, 3) || !checkRoles.run(client, userRole, subjectRole)) return;
-        if(message.member.roles.exists('name', args.join())) {
-            message.member.removeRole(message.guild.roles.find('name', args.join()));
-        } else {
-            message.member.addRole(message.guild.roles.find('name', args.join()));
+    if(args[0] === "help") { //Sends command info for help argument
+        const help = new Discord.RichEmbed()
+            .setTitle("Give Role")
+            .setColor(0xffdf00)
+            .setDescription("Gives a specified role to the user")
+            .addField("Usage", `${config.prefix}giverole {roleName}`);
+        message.channel.send(help);
+        return;
+    }
+    if(message.mentions.members.first() != undefined) {
+        var member = message.mentions.members.firts()
+        delete args[0]; 
+        var userRole = message.member.highestRole;
+        var subjectRole = message.guild.roles.find('name', args.join());
+        if(!permissionLevel.run(client, message, 3) || !checkRoles.run(client, userRole, subjectRole)) return;
+            if(member.roles.exists('name', args.join())) {
+                member.removeRole(message.guild.roles.find('name', args.join()));
+            } else {
+                member.addRole(message.guild.roles.find('name', args.join()));
+            }
         }
+    } else {
+        var userRole = message.member.highestRole;
+        var subjectRole = message.guild.roles.find('name', args.join());
+        if(!permissionLevel.run(client, message, 3) || !checkRoles.run(client, userRole, subjectRole)) return;
+            if(message.member.roles.exists('name', args.join())) {
+                message.member.removeRole(message.guild.roles.find('name', args.join()));
+            } else {
+                message.member.addRole(message.guild.roles.find('name', args.join()));
+            }
+        }
+    }
 }
