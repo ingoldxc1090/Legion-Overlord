688cbf0012ffd9335cdff9d1d07e821c9486a4b8
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-12-10 18:52:16 -0500

    good code

--- commands/bitcoin.js
+++ commands/bitcoin.js
@@ -23,32 +23,38 @@ exports.run = (client, message, args) => {
             price = price.split(",");
             price = price.join("");
             price = Number(price);
-            https.get(`https://bitcoin.miningpoolhub.com/index.php?page=api&action=getuserbalance&api_key=41d8772cf070e6e22f5b0d0bf083d675979bd4bf616cc201c54955f292900cf3&id=183868`, (resp) = > { //Makes a get request to International Chuck Norris Database API
-                let data = '';
 
-                resp.on('data', (chunk) = > { //Writes data chunks to variable as they are recieved
-                    data += chunk;
-                });
+    https.get(`https://bitcoin.miningpoolhub.com/index.php?page=api&action=getuserbalance&api_key=22af77ff8d67855803d340659f249743bd3346337681d8c897686f3c0787daab&id=192297`, (resp) => { //Makes a get request to International Chuck Norris Database API
+        let data = '';
 
-                resp.on('end', () = > {
-                    var balance = Number(JSON.parse(data).getuserbalance.data.confirmed);
-                    var balanceUSD = Number(Math.round((balance * price) + 'e2') + 'e-2');
-                    message.channel.send(`Gary's bitcoin mining balance is ${balance} BTC or $${balanceUSD}.`);
-                });
-            }).on("error", (err) = > {
-                console.error(err);
-            });
-            https.get(`https://bitcoin.miningpoolhub.com/index.php?page=api&action=getuserbalance&api_key=22af77ff8d67855803d340659f249743bd3346337681d8c897686f3c0787daab&id=192297`, (resp) = > { //Makes a get request to International Chuck Norris Database API
-                let data = '';
+    resp.on('data', (chunk) => { //Writes data chunks to variable as they are recieved
+        data += chunk;
+});
+    resp.on('end', () => {
+        var balance = Number(JSON.parse(data).getuserbalance.data.confirmed);
+        console.log(balance);
+    var balanceUSD = Math.round((balance * price) + 'e2') + 'e-2';
+    message.channel.send(`Cam's bitcoin mining balance is ${balance} BTC or $${balanceUSD}.`);
+});
+}).on("error", (err) => {
+        console.error(err);
+});
+            https.get(`https://bitcoin.miningpoolhub.com/index.php?page=api&action=getuserbalance&api_key=41d8772cf070e6e22f5b0d0bf083d675979bd4bf616cc201c54955f292900cf3&id=183868`, (resp) => { //Makes a get request to International Chuck Norris Database API
+        let data = '';
 
-                resp.on('end', () = > {
-                    var balance = JSON.parse(data).data.confirmed;
-                    var balanceUSD = Math.round((balance * price) + 'e2') + 'e-2';
-                    message.channel.send(`Cam's bitcoin mining balance is ${balance} BTC or $${balanceUSD}.`);
-                });
-            }).on("error", (err) = > {
-                console.error(err);
-            });
+    resp.on('data', (chunk) => { //Writes data chunks to variable as they are recieved
+        data += chunk;
+});
+
+    resp.on('end', () => {
+        var balance = Number(JSON.parse(data).getuserbalance.data.confirmed);
+    var balanceUSD = Number(Math.round((balance * price) + 'e2') + 'e-2');
+    message.channel.send(`Gary's bitcoin mining balance is ${balance} BTC or $${balanceUSD}.`);
+});
+}).on("error", (err) => {
+        console.error(err);
+});
+
         });
     }).on("error", (err) => {
         console.error(err);
